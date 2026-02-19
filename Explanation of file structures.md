ea0431b2b12e8df214c4505d4fda2a59c3b2659a
Feian-osa <karkidipeshhuu555@gmail.com>
2026-02-19 22:36:49 +0545

    added K_means clustering for customer segmentation and streamlit dashboard for rfm values in tables , pie chart and scatterplot

--- Explanation of file structures.md
+++
@@ -1,6 +0,0 @@
-# \### **Explanation of Structure**
-
-* *notebooks → Step-by-step Jupyter notebooks for cleaning, analysis, KPI calculation, and final dashboard.*  
-
-* *reports → Stores exported figures and final report PDFs for presentations or submissions.*  
-* *\*README.md → Project summary, tools used, file structure explanation for anyone visiting the repo.*  
---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st 
+import pandas as pd 
+import plotly.express as px 
+
+# Load Excel file
+rfm = pd.read_excel("category_summary.xlsx")  # make sure the file is in the same folder as app.py
+
+st.title("Superstore RFM Dashboard")
+st.dataframe(rfm)
+fig_pie = px.pie(rfm, 
+                 names='Cluster', 
+                 values='Customer_Count', 
+                 title='Customer Count per Cluster')
+st.plotly_chart(fig_pie)
+
+import plotly.express as px
+
+fig_scatter = px.scatter(
+    rfm,
+    x='Recency_Mean',       # use Recency_Mean instead of Recency
+    y='Monetary_Mean',      # use Monetary_Mean
+    size='Frequency_Mean',  # use Frequency_Mean
+    color='Cluster',
+    hover_data=['Recency_Mean', 'Frequency_Mean', 'Monetary_Mean']
+)
+
+st.plotly_chart(fig_scatter)
+
+
+
+
+##open terminal and run the following command to start the Streamlit app:
+# pip install streamlit 
+# pip install plotly
+# streamlit run "app.py"(the terminal should be in the same directory as app.py)
+#install streamlit and plotly if you haven't already:
+
